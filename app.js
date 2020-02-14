d672c19819b9b3128edaed9ef1b11ef28451a6c9
Emmet <emmetlawton@gmail.com>
2020-02-14 13:53:03 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var path = require('path');
+const app = express();
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT 
+app.get('/', (req, res) => {
+    res.render('etch5.html');
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
